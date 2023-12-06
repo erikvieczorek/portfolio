cea5074fe4a9e812cb3f1fc1feee71793597ae06
erikvieczorek <erikvieczorek@gmail.com>
2023-12-06 15:07:21 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/erikvieczorek.png" />
+    <Titulo fontSize={20}>Erik Vieczorek</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,21 @@
+import Titulo from '../../components/Titulo'
+import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
+
+const Sobre = () => (
+  <section>
+    <Titulo fontSize={16}>Sobre mim</Titulo>
+    <Paragrafo tipo="secundario">
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque
+      accusantium numquam non dignissimos atque sequi minus! Incidunt
+      consequuntur, maiores exercitationem sed adipisci quis commodi culpa
+      excepturi at ducimus, deleniti et.
+    </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=erikvieczorek&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=erikvieczorek&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
+  </section>
+)
+
+export default Sobre
