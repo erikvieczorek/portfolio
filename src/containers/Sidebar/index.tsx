bc1611cd0d11adf0c0048b67819b83fed1ae7268
erikvieczorek <erikvieczorek@gmail.com>
2023-12-07 10:30:16 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,21 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/erikvieczorek.png" />
-    <Titulo fontSize={20}>Erik Vieczorek</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Erik Vieczorek</Titulo>
+      <Paragrafo tipo="secundario" font-size={16}>
+        erikvieczorek
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
